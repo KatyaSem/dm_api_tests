39a9bb3d486d116716a83662628b5b2b64e42778
KatyaSem <see_mona@mail.ru>
2026-02-10 00:36:43 +0300

    Добавлен proxy-клиент

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,10 +1,20 @@
-
 from dm_api_account.apis.account_api import AccountApi
+import structlog
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(
+            indent=4,
+            ensure_ascii=True,
+            #sort_keys=True
+        )
+    ]
+)
 
 def test_post_v1_account():
     # Регистрация пользователя
     account_api = AccountApi(host = 'http://185.185.143.231:5051')
-    login = 'katya_test47'
+    login = 'katya_test61'
     email = f'{login}@mail.ru'
     password = '123456'
 
@@ -15,8 +25,6 @@ def test_post_v1_account():
     }
 
     response = account_api.post_v1_account(json_data = json_data)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 201, f"Пользователь не был создан{response.json()}"
 
 
