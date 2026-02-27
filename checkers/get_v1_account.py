5647d7f58d91029e856b5e9fe7f4c1c59b874ea1
KatyaSem <see_mona@mail.ru>
2026-02-27 09:00:41 +0300

    добавлены функции-чекеры

---
+++ checkers/get_v1_account.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+from hamcrest import (
+    assert_that,
+    all_of,
+    has_property,
+    starts_with,
+    instance_of,
+    has_properties,
+    equal_to,
+)
+
+
+class GetV1Account:
+
+
+    @classmethod
+    def check_response_values(
+            cls,
+            response
+    ):
+        assert_that(
+            response,
+            all_of(
+                has_property('resource', has_property('login', starts_with("katya_test"))),
+                has_property('resource', has_property('registration', instance_of(datetime))),
+                has_property(
+                    'resource', has_properties(
+                        {
+                            'rating': has_properties(
+                                {
+                                    "enabled": equal_to(True),
+                                    "quality": equal_to(0),
+                                    "quantity": equal_to(0)
+                                }
+                            )
+                        }
+                    )
+                ),
+                has_property('resource', has_property('roles', instance_of(list))),
+                has_property('resource', has_property('online', instance_of(datetime)))
+            )
+        )
--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,42 +1,18 @@
+from assertpy import assert_that, soft_assertions
+from checkers.get_v1_account import GetV1Account
 from checkers.http_checkers import check_status_code_http
 from datetime import datetime
-from hamcrest import (
-    assert_that,
-    has_property,
-    starts_with,
-    all_of,
-    instance_of,
-    has_properties,
-    equal_to
-)
-
+from dm_api_account.models.user_details_envelope import UserRole
 
 
 def test_get_v1_account_auth(auth_account_helper):
     response = auth_account_helper.get_user(validate_response=True)
-    assert_that(
-        response,
-        all_of(
-            has_property('resource', has_property('login', starts_with("katya_test"))),
-            has_property('resource', has_property('registration', instance_of(datetime))),
-            has_property(
-                'resource', has_properties(
-                    {
-                        'rating': has_properties(
-                            {
-                                "enabled": equal_to(True),
-                                "quality": equal_to(0),
-                                "quantity": equal_to(0)
-                            }
-                        )
-                    }
-                )
-            ),
-            has_property('resource', has_property('roles', instance_of(list))),
-            has_property('resource', has_property('online', instance_of(datetime)))
-        )
-    )
+    with soft_assertions():
+        assert_that(response.resource.login).is_equal_to("katya_test")
+        assert_that(response.resource.online).is_instance_of(datetime)
+        assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
 
+    GetV1Account.check_response_values(response)
 
 def test_get_v1_account_no_auth(account_helper):
     with check_status_code_http(401, "User must be authenticated"):
