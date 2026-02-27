d3771ce9cffb6c59653bd71aa8ed85ce38532a7c
KatyaSem <see_mona@mail.ru>
2026-02-27 20:27:54 +0300

    Update checkers/post_v1_account_login.py
    Co-authored-by: Valery Menshikov <team095hunter@gmail.com>

--- checkers/post_v1_account_login.py
+++ checkers/post_v1_account_login.py
@@ -22,7 +22,7 @@ class PostV1AccountLogin:
         today = datetime.now().strftime('%Y-%m-%d')
         assert_that(
             response, all_of(
-                has_property('resource', has_property('login', starts_with("katya_test"))),
+                has_property('resource', has_property('login', starts_with(login))),
                 has_property('resource', has_property('registration', instance_of(datetime))),
                 has_property(
                     'resource', has_properties(
